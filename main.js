0389435373580b68eacc7d093adf2a6364b12cb7
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-19 20:03:11 +0400

    feat: animation of circle

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
-let text = 'string';
+'use strict';
+const start = document.querySelector('.start'),
+  reset = document.querySelector('.reset'),
+  elem = document.querySelector('.elem');
 
-function getStr (arg) {
-  if (typeof arg !== 'string') {
-    alert('Переменная не является строкой');
-  } else {
-    if (arg.length > 30) {
-      console.log(arg.substring(0, 30) + '...');
-    } else {
-      console.log( arg.trim());
-    }
+
+let interval,
+count = 0;
+// const getRandom = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+
+const moveCicrle = () => {
+  count++;
+  console.log('count: ', count);
+
+  if (count < 500) {
+    elem.style.left = `${count}px`;
+    elem.style.top = `${count}px`;
+  } else if (count === 500) {
+    // count = 0;
+    elem.style.left = `${500}px`;
+    elem.style.top = `${500}px`;
+  } else if (count > 500) {
+    elem.style.top = `${1000 - count}px`;
+  } else if (count === 900) {
+    elem.style.top = `${900}px`;
+  } else if (count > 900) {
+    elem.style.left = `${count}px`;
   }
-}
 
-getStr(text);
+  interval = requestAnimationFrame(moveCicrle);
+};
+
+let animate = false;
+start.addEventListener('click', () => {
+    if (animate) {
+      interval = requestAnimationFrame(moveCicrle);
+      animate = false;
+    } else {
+      animate = true;
+      cancelAnimationFrame(interval);
+    }
+});
+
+reset.addEventListener('click', () => {
+  count = 0;
+  cancelAnimationFrame(interval);
+  
+  elem.style.left = 10 + '%';
+  elem.style.top = 10 + '%';
+});
