2b2280ffc401a3782d2a8c667bc898df6a203522
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-19 13:03:52 +0400

    feat: was done task 4

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
-let lang;
-lang = 'en';
-if (lang === 'ru') {
-  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+let text = 'string';
+
+function getStr (arg) {
+  if (typeof arg !== 'string') {
+    alert('Переменная не является строкой');
+  } else {
+    if (arg.length > 30) {
+      console.log(arg.substring(0, 30) + '...');
+    } else {
+      console.log( arg.trim());
+    }
+  }
 }
 
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-  
-  case 'en': 
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-  break;
-
-  default: console.log('Не выбран язык');
-    break;
-}
-
-let week = [lang === 'ru'? ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'] :
- ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']];
-
- console.log(week);
-
- let namePerson = 'Lnztk';
-
- namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+getStr(text);
