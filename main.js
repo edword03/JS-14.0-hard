d49b8b170610c3aa4feec495a3e654d0207098da
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-19 12:47:51 +0400

    feat: added terms

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const num = 266219;
+let lang;
+lang = 'en';
+if (lang === 'ru') {
+  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
 
-console.log(String(num).split('').reduce((sum, current) => {
-  return sum * current;
-})**3);
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+    break;
+  
+  case 'en': 
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+  break;
 
+  default: console.log('Не выбран язык');
+    break;
+}
+
+let week = [lang === 'ru'? ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'] :
+ ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']];
+
+ console.log(week);
+
+ let namePerson = 'Lnztk';
+
+ namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
